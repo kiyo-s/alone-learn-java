29a6ca56e35ecf8e0614a731d25908b564068aa0
Kyohei Saito <k01207s@gmail.com>
2024-06-30 18:47:41 +0900

    hello class に時間を表示するコードを追加

--- selfjava/src/to/msn/wings/selfjava/chap01/Hello.java
+++ selfjava/src/to/msn/wings/selfjava/chap01/Hello.java
@@ -1,5 +1,10 @@
 package to.msn.wings.selfjava.chap01;
 
-public class Hello {
+import java.time.LocalDateTime;
 
+public class Hello {
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+    }
 }
