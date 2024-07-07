339ae4d308bfbd668005cd3a77f94ce259a89ca6
Kyohei Saito <k01207s@gmail.com>
2024-07-07 16:11:23 +0900

    数値と非数値が混在する算術演算子

---
+++ selfjava/src/to/msn/wings/selfjava/chap03/NoNumber.java
@@ -0,0 +1,14 @@
+package to.msn.wings.selfjava.chap03;
+
+import java.time.LocalDateTime;
+
+public class NoNumber {
+    public static void main(String[] args) {
+        System.out.println(1 + 2);
+        System.out.println("a" + 5);
+        System.out.println(5 + "b");
+        System.out.println(5.0 + 3.1);
+        System.out.println("1" + "2");
+        System.out.println("now:" + LocalDateTime.now());
+    }
+}
