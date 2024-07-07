3778c9fc925537c127e07a34dc81308ef211807b
Kyohei Saito <k01207s@gmail.com>
2024-07-07 15:48:41 +0900

    Add practice 1 and 3

---
+++ selfjava/src/to/msn/wings/selfjava/chap02/practice.java
@@ -0,0 +1,8 @@
+package to.msn.wings.selfjava.chap02;
+
+public class practice {
+    public static void main(String[] args) {
+        var data = "Hello, world !";
+        System.out.println(data);
+    }
+}
