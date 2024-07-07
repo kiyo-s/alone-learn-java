f46eea5bd6d292bf253f892254e241c22248902b
Kyohei Saito <k01207s@gmail.com>
2024-07-07 16:30:35 +0900

    zero 除算

---
+++ selfjava/src/to/msn/wings/selfjava/chap03/DivZero.java
@@ -0,0 +1,10 @@
+package to.msn.wings.selfjava.chap03;
+
+public class DivZero {
+    public static void main(String[] args) {
+        // System.out.println(5 / 0); ERROR
+        // System.out.println(5 % 0); ERROR
+        System.out.println(5d / 0);
+        System.out.println(5d % 0);
+    }
+}
