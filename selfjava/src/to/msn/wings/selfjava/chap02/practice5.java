44228f68275b9a0a7e6d3e6daadb0a67c1ee00e1
Kyohei Saito <k01207s@gmail.com>
2024-07-07 15:55:20 +0900

    Add practice 5

---
+++ selfjava/src/to/msn/wings/selfjava/chap02/practice5.java
@@ -0,0 +1,22 @@
+package to.msn.wings.selfjava.chap02;
+
+public class practice5 {
+    public static void main(String[] args) {
+        var value = 10.0;
+        System.out.println(value);
+
+        System.out.println("Welcome, \nJava world !");
+
+        String str = null;
+        System.out.println(str);
+
+        int[][] data = new int[5][4];
+
+        var list = new int[][] {
+                { 2, 3, 5 },
+                { 1, 2 },
+                { 10, 11, 12, 13 },
+        };
+        System.out.println(list[1][1]);
+    }
+}
